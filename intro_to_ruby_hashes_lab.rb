9a86e2563f9c8969e2882adf2e8de358005ca788
Karla R <karla.y.reyes12@gmail.com>
2018-07-13 14:06:07 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	    {pieces: 4,
+	 {:railroads => {pieces: 4,
 			names: {
 				reading_railroad: {},
 				pennsylvania_railroad: {},
